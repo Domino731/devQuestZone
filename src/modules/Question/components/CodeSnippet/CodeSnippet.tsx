46b45bebcf76ba7a22d652c36408b0f8689b56a0
Domino731 <d.orzechowski18@gmail.com>
2023-11-12 16:13:06 +0100

    feature - CodeSnippet - coping text

--- src/modules/Question/components/CodeSnippet/CodeSnippet.tsx
+++ src/modules/Question/components/CodeSnippet/CodeSnippet.tsx
@@ -8,22 +8,34 @@ import {getLangColor, getLangHeaderTextColor} from "../../../../utils/getLangCol
 import Typography from "@mui/material/Typography";
 import {ContentCopy} from "@mui/icons-material";
 import IconButton from '@mui/material/IconButton';
+import {useCallback, useRef} from "react";
 
 // Register the language
 hljs.registerLanguage('javascript', javascript);
 export const CodeSnippet = () => {
+    const ref = useRef<HTMLDivElement>(null);
 
+    const handleCopyCode = useCallback(() => {
+        // TODO: display notification after copy
+        if (ref.current?.innerText != null) {
+            navigator.clipboard.writeText(ref.current?.innerText)
+        }
+    }, [])
 
     return (
         <div className={styles.container}>
             <Box sx={{backgroundColor: getLangColor('javaScript')}} className={styles.header}>
                 <Typography variant="subtitle2" sx={{color: getLangHeaderTextColor('javaScript')}}>JavaScript</Typography>
-                <IconButton sx={{color: getLangHeaderTextColor('javaScript'), fontSize: '18px'}}>
+                <IconButton
+                    sx={{color: getLangHeaderTextColor('javaScript'), fontSize: '18px'}}
+                onClick={handleCopyCode}
+                >
                     <ContentCopy fontSize="inherit"/>
                 </IconButton>
             </Box>
 
-            <SyntaxHighlighter language="javascript" style={dracula}>
+            <div  ref={ref}>
+                <SyntaxHighlighter language="javascript" style={dracula}>
        {
            `
 export function useWeb3AnalyticsReporter() {
@@ -44,6 +56,8 @@ export function useWeb3AnalyticsReporter() {
 }`
        }
 </SyntaxHighlighter>
+            </div>
+
         </div>
     );
 };
