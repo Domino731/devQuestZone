648ff0a5bccde268c032f8352ef8d840ea592230
Domino731 <d.orzechowski18@gmail.com>
2023-11-14 18:50:07 +0100

    feature - firebase - config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import {Question} from "./modules/Question";
 import {QuestionList} from "./modules/QuestionsList";
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css'
+import {firebaseApp} from "./firebase/config.ts";
+
 
 function App() {
     return (
