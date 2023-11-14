545e24e7f9d53364a93c1ef98158edba96ebe03e
Domino731 <d.orzechowski18@gmail.com>
2023-11-14 18:31:02 +0100

    feature - questions list - simple list

--- src/modules/QuestionsList/QuestionList.tsx
+++ src/modules/QuestionsList/QuestionList.tsx
@@ -1,9 +1,44 @@
 import styles from './styles.module.scss';
 import Box from "@mui/material/Box";
 import {Header} from "../Header";
+import Typography from "@mui/material/Typography";
 
 export const QuestionList = () => {
     return <Box bgcolor="primary.main" className={styles.container}>
         <Header/>
+        <ul className={styles.list}>
+            <li>
+                <Box border="1px solid" borderColor="primary.light" bgcolor="primary.light" borderRadius={4}
+                     className={styles.listItemContent}>
+                    <Typography variant="h6">Explain flux architecture in terms of react redux library. Do you know
+                        other libraries
+                        for state management for react?</Typography>
+                </Box>
+            </li>
+            <li>
+                <Box border="1px solid" borderColor="primary.light" bgcolor="primary.light" borderRadius={4}
+                     className={styles.listItemContent}>
+                    <Typography variant="h6">Explain flux architecture in terms of react redux library. Do you know
+                        other libraries
+                        for state management for react?</Typography>
+                </Box>
+            </li>
+            <li>
+                <Box border="1px solid" borderColor="primary.light" bgcolor="primary.light" borderRadius={4}
+                     className={styles.listItemContent}>
+                    <Typography variant="h6">Explain flux architecture in terms of react redux library. Do you know
+                        other libraries
+                        for state management for react?</Typography>
+                </Box>
+            </li>
+            <li>
+                <Box border="1px solid" borderColor="primary.light" bgcolor="primary.light" borderRadius={4}
+                     className={styles.listItemContent}>
+                    <Typography variant="h6">Explain flux architecture in terms of react redux library. Do you know
+                        other libraries
+                        for state management for react?</Typography>
+                </Box>
+            </li>
+        </ul>
     </Box>
 }
