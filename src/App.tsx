b5b2d1a0f9aaf95aeac30af9cc9ff93647964f0f
Domino731 <d.orzechowski18@gmail.com>
2023-11-14 19:01:00 +0100

    feature - firebase - testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@ import {Question} from "./modules/Question";
 import {QuestionList} from "./modules/QuestionsList";
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css'
-import {firebaseApp} from "./firebase/config.ts";
-
+import {firestore} from "./firebase/config.ts";
+import {doc, getDoc} from "firebase/firestore";
+import {useEffect} from "react";
+import {collection, getDocs} from "firebase/firestore";
 
 function App() {
+    useEffect(() => {
+        const test = async () => {
+            const querySnapshot = await getDocs(collection(firestore, "javascript"));
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+            });
+        }
+        test();
+    }, [])
     return (
         <CssBaseline>
             <div style={{display: 'flex'}}>
