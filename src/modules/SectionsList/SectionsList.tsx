b7ee165fcf2fe5ff318803bf01541ca1bb819d3e
Domino731 <d.orzechowski18@gmail.com>
2023-11-18 10:46:13 +0100

    feature - sections list - simple list

--- src/modules/SectionsList/SectionsList.tsx
+++ src/modules/SectionsList/SectionsList.tsx
@@ -4,6 +4,12 @@ import {useAppDispatch, useAppSelector} from "../../store.ts";
 import {sectionsListSelectors} from "./store/slice.selectors.ts";
 import {useEffect} from "react";
 import {sectionListActions} from "./store/slice.actions.ts";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import styles from './styles.module.scss';
 
 export const SectionsList = ({sectionList}: SectionListProps) => {
     const dispatch = useAppDispatch();
@@ -20,7 +26,30 @@ export const SectionsList = ({sectionList}: SectionListProps) => {
     console.log(sectionsList);
     // console.log(isLoading);
 
-    return <Container>
-        {sectionList}
+    if (!sectionsList) {
+        // TODO loader
+        return 'loading...'
+    }
+
+    return <Container className={styles.container}>
+        <ul className={styles.list}>
+            {sectionsList.map(({name, docId}) => <li className={styles.listItem} key={docId}>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon/>}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography>{name}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </li>)}
+        </ul>
     </Container>
 }
